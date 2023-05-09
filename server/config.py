4bd6cfef6f9bdb0837774846153cfa7dcd905984
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-05-09 07:18:37 -0400

    5

--- server/config.py
+++ server/config.py
@@ -7,7 +7,8 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-
+from dotenv import load_dotenv
+load_dotenv()
 # Local imports
 
 # Instantiate app, set attributes
