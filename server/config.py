2d43e32103536a36204a112073b6b085566c9a84
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-09-04 17:33:48 -0400

    21

--- server/config.py
+++ server/config.py
@@ -25,7 +25,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 # Define metadata, instantiate db
-# metadata = MetaData(naming_convention={bb
+# metadata = MetaData(naming_convention={
 #     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 # })
 db = SQLAlchemy()
